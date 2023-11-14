43f929ef2a1e64e136643f890fc75771f43798c0
Axel <axel.nie17@gmail.com>
2023-11-14 10:22:20 +0100

    added gif

--- functions/helper_images.jl
+++ functions/helper_images.jl
@@ -1,10 +1,26 @@
-using Pkg
-Pkg.add("Images")
-Pkg.add("FileIO")
+using Images, FileIO
 
 function display_images()
 
-    print("display_images")
+    folder_path = "./data"
+    new_extension = ".gif"
+    files = readdir(folder_path)
+
+    for image_file in files
+        image_path = joinpath(folder_path, image_file)
+        image = load(image_path)
+        display(image)
+    end
+end
+
+function append_extension_to_files(folder_path, new_extension=".gif")
+    files = readdir(folder_path)
+
+    for file in files
+        old_path = joinpath(folder_path, file)
+        new_path = old_path * new_extension
+        mv(old_path, new_path)
+    end
 end
 
 # Function to load images from a directory
